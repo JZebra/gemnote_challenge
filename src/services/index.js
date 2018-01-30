04cedec114043766cf087cd76531da4e8ab00e3c
John Shen <johnzhihaoshen@gmail.com>
2018-01-30 15:54:18 -0800

    feathers generate service for products

--- src/services/index.js
+++ src/services/index.js
@@ -1,2 +1,4 @@
-module.exports = function (app) { // eslint-disable-line no-unused-vars
+const products = require('./products/products.service.js');
+module.exports = function (app) {
+  app.configure(products);
 };
